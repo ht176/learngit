ea2016bdcb0b112728cc501522ed4d0a0001583e
hetong <1156385866@qq.com>
2021-04-16 17:42:30 +0800

    更新设计模式

--- javascriptStudyRecord.js
+++ javascriptStudyRecord.js
@@ -1,4 +1,4 @@
-/* 
+/*
 页面重构怎么操作？
 
 网站重构：在不改变外部行为的前提下，简化结构、添加可读性，而在网站前端保持一致的行为。
@@ -29,6 +29,7 @@ HTTP服务器的文件缓存 */
 /** 校验X,Y是否相等
  *  封装方法比对两个对象是否相等,使用了instanceof(检测对象的类型)、Object.keys(返回对象属性数组)、递归方法
  */
+/*
 function equals (x, y) {
   var f1 = x instanceof Object
   var f2 = y instanceof Object
@@ -55,10 +56,11 @@ function equals (x, y) {
   return true
 }
 console.log(equals([12, 12], [12, 13]))
-
+ */
 /** 闭包
  * 在一个函数A中定义一个函数B,B中引用了A中的变量,A返回B的引用,在别的地方调用A.
  */
+/*
 function say () {
   let num = 666
   return function () {
@@ -72,3 +74,121 @@ function test () {
   sayConsole()
   sayConsole()
 }
+*/
+/** 工厂模式
+ *
+ */
+/*
+class MobilePhoneFactory {
+  // 提供操作系统的接口
+  CreateOS () {
+    throw new Error('抽象工厂方法不允许直接调用，你需要将我重写！')
+  }
+  // 提供硬件的接口
+  CreateHardWare () {
+    throw new Error('抽象工厂方法不允许直接调用，你需要将我重写！')
+  }
+}
+
+// 定义操作系统的抽象产品类
+class OS {
+  controlHardWare () {
+    throw new Error('抽象产品方法不允许直接调用，你需要将我重写！')
+  }
+}
+
+// 定义具体操作系统的具体产品类
+class AndroidOS extends OS {
+  controlHardWare () {
+    console.log('我会用安卓的方式去操作硬件')
+  }
+}
+class AppleOS extends OS {
+  controlHardWare () {
+    console.log('我会用apple的方式去操作硬件')
+  }
+}
+
+// 定义手机硬件的抽象产品类
+class HardWare {
+  // 手机硬件的共性方法，这里提取了“根据命令运转”这个共性
+  operateByOrder () {
+    throw new Errow('抽象产品方法不允许直接调用，你需要将我重写！')
+  }
+}
+
+// 定义具体硬件的具体产品类
+class QualcommHardWare extends HardWare {
+  operateByOrder () {
+    console.log('我会用高通的方式去运转')
+  }
+}
+class MiWare extends HardWare {
+  operateByOrder () {
+    console.log('我会用小米的方式去运转')
+  }
+}
+
+// 具体工厂继承自抽象工厂
+class FakeStarFactory extends MobilePhoneFactory {
+  CreateOS () {
+    // 提供安卓系统实例
+    return new AndroidOS()
+  }
+  CreateHardWare () {
+    // 提供高通硬件实例
+    return new QualcommHardWare()
+  }
+}
+
+// 实例化我的手机
+const myPhone = new FakeStarFactory()
+// 让他拥有操作系统
+const myOS = myPhone.CreateOS()
+// 让他拥有硬件
+const myHardWare = myPhone.CreateHardWare()
+// 启动操作系统
+myOS.controlHardWare()
+// 唤醒硬件
+myHardWare.operateByOrder()
+
+// 关键的时刻来了——假如有一天，FakeStar过气了，我们需要产出一款新机投入市场，这时候怎么办？我们是不是不需要对抽象工厂MobilePhoneFactory做任何修改，只需要拓展它的种类：
+class newStarFactory extends MobilePhoneFactory {
+  createOS () {
+    // 操作系统实现代码
+  }
+  createHardWare () {
+    // 硬件实现代码
+  }
+}
+*/
+// 定义Storage
+class Storage {
+  static getInstance () {
+    // 判断是否已经new过1个实例
+    if (!Storage.instance) {
+      // 若这个唯一的实例不存在，那么先创建它
+      Storage.instance = new Storage()
+    }
+    // 如果这个唯一的实例已经存在，则直接返回
+    return Storage.instance
+  }
+  getItem (key) {
+    return localStorage.getItem(key)
+  }
+  setItem (key, value) {
+    return localStorage.setItem(key, value)
+  }
+}
+
+const storage1 = Storage.getInstance()
+const storage2 = Storage.getInstance()
+
+storage1.setItem('name', '李雷')
+// 李雷
+storage1.getItem('name')
+// 也是李雷
+storage2.getItem('name')
+
+// 返回true
+storage1 === storage2
